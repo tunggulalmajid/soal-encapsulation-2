4560e44e4a94389fe29344d06fd09b7a4e4815db
tunggulalmajid <tunggulmajid@gmail.com>
2025-03-28 22:13:00 +0700

    Add project files.

---
+++ soal encapsulation 2/Biodata.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soal_encapsulation_2
+{
+    internal class Biodata
+    {
+        public string nama, prodi;
+        private int _nim;
+
+        public Biodata(string nama, string prodi)
+        {
+            this.nama = nama;
+            this.prodi = prodi;
+        }
+        public int Nim
+        {
+
+            get { return _nim; }
+            set
+            {
+                prodi = prodi.ToLower();
+                if (prodi == "sistem informasi")
+                {
+                    if (value >= 1000 && value <= 1100)
+                    {
+                        _nim = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error bang");
+                    }
+               
+                }
+                else if (prodi == "teknologi informasi")
+                {
+                    if (value >= 2000 && value <= 2100)
+                    {
+                        _nim = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error bang");
+                    }
+               
+                }
+                else if (prodi == "informatika")
+                {
+                    if (value >= 3000 && value <= 3100)
+                    {
+                        _nim = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error bang");
+                    }
+                  
+                }
+            }
+        }
+    }
+}
---
+++ soal encapsulation 2/Program.cs
@@ -0,0 +1,10 @@
+﻿using soal_encapsulation_2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Biodata tunggul = new Biodata("tunggul", "teknologi informasi");
+        tunggul.Nim = 2058;
+    }
+}
